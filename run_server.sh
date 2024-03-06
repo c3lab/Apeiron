76be69218425394cf98e52774050b7c7d9b0214f
Walter Brescia <walter.brescia@poliba.it>
2024-03-06 15:52:46 +0100

    update hugo server

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker run --rm -it -p 1313:1313  -v $(pwd):/src klakegg/hugo:0.101.0-ext-alpine server
+# docker run --rm -it -p 1313:1313  -v $(pwd):/src klakegg/hugo:0.101.0-ext-alpine server --verbose
+docker run --rm -it --net host  -v $(pwd):/src -w /src betterweb/hugo:extended-0.121.1-20-1 -c "hugo server --verbose"
